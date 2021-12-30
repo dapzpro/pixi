6d0dc174d1946cc29e689268b3ddea401f54f8f0
dapzpro <dapzpro@gmail.com>
2021-12-30 12:48:49 -0500

    texto de muerte

--- juego/text.js
+++ juego/text.js
@@ -12,3 +12,27 @@ const lifeText = (width) => {
   text.y = 0;
   return text;
 };
+///lora
+const deadText = (height, width) => {
+  const style = new PIXI.TextStyle({
+    fontFamily: "Lora",
+    fontSize: 50,
+    fill: ["#750b0e"],
+    stroke: "#000000",
+    strokeThickness: 2,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 3,
+    dropShadowAngle: Math.PI / 3,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: "round",
+  });
+
+  const text = new PIXI.Text("YOU ARE DEAD", style);
+  text.x = width / 2;
+  text.y = height / 2;
+  text.anchor.set(0.5);
+  return text;
+};
